1bb6f1ba38dba543e9115ccc0956236d93a444d6
Mzwandile <mzwandileT21@gmail.com>
2023-12-11 22:43:33 -0800

    let us start

---
+++ src/bms/BMS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bms;
+
+/**
+ *
+ * @author Acer
+ */
+public class BMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //ghp_DshWRGyrt1gH5HcNfightDRy1fZ9db2GVBgh
+    }
+    
+}
